df2415be959bdb1d6987c508dc58e3861c3e22f1
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-09-02 23:22:19 +0900

    4-1-2

--- chp4/4-1-2.js
+++ chp4/4-1-2.js
@@ -10,3 +10,11 @@ function sumVariadic() {
     return Array.from(arguments).reduce(function (total, n) { return total + n; }, 0);
 }
 console.log(sumVariadic(1, 2, 3));
+function sub() {
+    var nums = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        nums[_i] = arguments[_i];
+    }
+    return Array.from(arguments).reduce(function (result, n) { return result - n; }, 100);
+}
+console.log(sub(10, 2, 4));
--- chp4/4-1-2.ts
+++ chp4/4-1-2.ts
@@ -9,3 +9,9 @@ function sumVariadic(...numbers: number[]): number {
 }
 
 console.log(sumVariadic(1, 2, 3))
+
+function sub(...nums: number[]) {
+	return Array.from(arguments).reduce((result, n) => result - n, 100)
+}
+
+console.log(sub(10, 2, 4))
