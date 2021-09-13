0e4bc733cf3cea497d6b751d7626122f0600e18a
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-09-14 00:00:19 +0900

    level1-3

---
+++ type-pra/level1/level1-3.js
@@ -0,0 +1,7 @@
+var isPositive2 = function (num) { return num >= 0; };
+// 使用例
+console.log(isPositive2(5));
+console.log(isPositive2(-34));
+// エラー例
+// isPositive2('foo');
+// const res: number = isPositive(123);
