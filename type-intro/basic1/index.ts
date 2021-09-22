a6111fd09c7bdb9a3fb47b30b90e9953499cec60
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-09-22 23:16:21 +0900

    test

--- type-intro/basic1/index.ts
+++ type-intro/basic1/index.ts
@@ -3,3 +3,6 @@ const f: (foo: string) => number = func;
 function func(arg: string): number {
 	return Number(arg);
 }
+
+const func2 = (foo: string, ...bar: number[]) => bar;
+console.log(func2(1,2,4));
