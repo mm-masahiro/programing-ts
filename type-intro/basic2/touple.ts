bfda1f54cbadd118822ce0ca67ca59f780f6dbd2
mm-masahiro <hand5.mm9.mzh@gmail.com>
2021-09-25 13:47:12 +0900

    touple2

--- type-intro/basic2/touple.ts
+++ type-intro/basic2/touple.ts
@@ -11,3 +11,18 @@ const strings: string[] = ['foo', 'bar', 'baz'];
 
 // 配列stringsの中身をfunc4の引数に展開して呼び出す
 console.log(func4(...strings));
+
+
+// 関数の引数列をジェネリクスで扱う
+function bind<T, U extends any[], R> (
+	func: (arg1: T, ...rest: U) => R,
+	value: T,
+): ((...args: U) => R) {
+	return (...args: U) => func(value, ...args);
+}
+
+const add1 = (x: number, y: number) => x + y;
+
+const add2 = bind(add1, 1);
+
+console.log(add2(5));
